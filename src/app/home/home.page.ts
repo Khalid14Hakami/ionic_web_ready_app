508b9d1e7c8d288eb8adc048f83209dd54b2dfc8
KHALID HAKAMI <khalid.hakami14@gmail.com>
2019-07-25 20:22:53 +0300

    create project

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  users = [1,2,3];
 
-  constructor() {}
-
+  constructor(private httpClient: HttpClient) {}
+  //   this.httpClient.get('https://randomuser.me/api/?results=20')
+  //     .subscribe(res => {
+  //       this.users = res['results'];
+  //       console.log(this.users);
+  //     },
+  //      error => {console.log(error); });
+  // }
 }
